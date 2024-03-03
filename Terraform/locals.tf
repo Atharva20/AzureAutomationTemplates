b044879eaf4d75d59c47c4c301b7bc8709088516
Atharva_Bhagade <batharva20@gmail.com>
2024-03-03 12:43:24 +0530

    updated yml deploy

--- Terraform/locals.tf
+++ Terraform/locals.tf
@@ -7,10 +7,10 @@ locals {
   rg_name = format("rg%s%s%s%s", "az", var.env, "sea", var.instance) // rgazdevsea01
 
   # Storage Account Name
-  storage_account_name = format("sa%s%s%s%s", "az", var.env, "sea", var.instance) // saazdevsea01
-  tfstate_container_name = format("tfstate%s%s%s%s", "az", var.env, "sea", var.instance) // tfstateazdevsea01
+  storage_account_name    = format("sa%s%s%s%s", "az", var.env, "sea", var.instance)       // saazdevsea01
+  tfstate_container_name  = format("tfstate%s%s%s%s", "az", var.env, "sea", var.instance)  // tfstateazdevsea01
   tfstate_container_name2 = format("tfstate2%s%s%s%s", "az", var.env, "sea", var.instance) // tfstateazdevsea01
-  
+
   # Source VirtualNetwork Name
   vnet_name = format("vnet-%s-%s-%s-%s", "az", var.env, "sea", var.instance)
 
